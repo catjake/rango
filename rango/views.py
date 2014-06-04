c0e93d62d50a54f601da97e5da5f458735b2cb48
catjake <jacob.keezel@gmail.com>
2014-06-04 17:07:10 -0400

    added templates, static files, and  media server

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,24 @@
 # coding=utf-8
-from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from time import time, localtime
+
+
+def _process_request(request, context_dict=None, fn=None):
+    """
+    :param request:
+    :param context_dict: optional dictionary passed to func:render_to_response
+    :param fn: html file from templates
+    :return: HttpResponse Object
+    """
+    # Request the context of the request
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Return the rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note the the first parameter is the template we wish to use.
+    return render_to_response(fn, context_dict, context)
 
 
 def index(request):
@@ -7,11 +26,10 @@ def index(request):
     :param request:
     :return:
     """
-    request = HttpResponse()
-    request.write("<p>Rango says hello and happy New Year!</p>")
-    request.write("<p>Links:<br>")
-    request.write("<a href=/rango/about>    About</a></p>")
-    return request
+    # Construct a dictionary to pass to the template engine as it's context
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template: rango/index.html
+    context_dict = {"boldmessage": "I am bold font from the context"}
+    return _process_request(request, context_dict, r"rango/index.html")
 
 
 def about(request):
@@ -19,7 +37,21 @@ def about(request):
     :param request:
     :return:
     """
-    request = HttpResponse()
-    request.write("<p>Rango says: Here is the about page</p>")
-    request.write("<p><a href=/rango>Back to Main</a></p>")
-    return request
+    # Construct a dictionary to pass to the template engine as it's context
+    # Note the value from key timestamp is an instance of GetLocalTime, and "now" is an attribute of that class
+    # {{ timestamp.now }} in the template: rango/about.html returns the localtime
+    context_dict = {"timestamp": GetLocalTime()}
+    return _process_request(request, context_dict, r"rango/about.html")
+
+
+class SilentAssertionError(Exception):
+    silent_variable_failure = True
+
+
+class GetLocalTime(object):
+    def now(self):
+        try:
+            t = localtime(time())
+            return( '%04d-%02d-%02d %02d:%02d:%02d'%(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec) )
+        except SilentAssertionError:
+            return "ooops"
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,7 @@
 # coding=utf-8
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +19,11 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r"^rango/", include("rango.urls")), # Add mapping from rango app: urls.py
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        "django.views.static",
+        (r"media/(?P<path>.*)",
+        "serve",
+        {"document_root": settings.MEDIA_ROOT}),
+    )
